4ff89ddaafd6ac8507d409de3b5ccc20a83c236f
walkerdorris <walker.dorris@gmail.com>
2015-11-07 14:15:43 -0600

    grabbing weather data

--- javascripts/getData.js
+++ javascripts/getData.js
@@ -13,17 +13,22 @@ define(["jquery", "lodash", "hbs", "bootstrap", "q", "firebase"], function($, _,
 
 
 	// var getWeather = {};
-	var zipcode = $("#zipCode").val();
+	var zipcode = 37207;
 
 	$.ajax({
-   		url: "api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us",
+   		url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us" + "&APPID=d918f1afe69b26f495f2dc4f6e949d73",
   		data: {
       		format: 'json'
    			  },
+   		done: function(data2){
+   			console.log(data2);
+   		},
    		error: function() {
-      		(console.log("Error retrieving data"));
+      		console.log("Error");
    			  },
    		type: 'GET'
 	});
+
+});
 	
 	
