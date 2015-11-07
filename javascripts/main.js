81bb6d780567462511202332e70ce7c875355706
walkerdorris <walker.dorris@gmail.com>
2015-11-07 11:58:46 -0600

    main setup

---
+++ javascripts/main.js
@@ -0,0 +1,21 @@
+requirejs.config({
+  baseUrl: './javascripts',
+  paths: {
+    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'lodash': '../lib/bower_components/lodash/lodash.min',
+    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'q': '../lib/bower_components/q/q',
+    'firebase' : '../lib/bower_components/firebase/firebase'
+  },
+  shim: {
+    'bootstrap': ['jquery'],
+    'firebase': {exports: 'Firebase'}
+  }
+});
+
+requirejs(
+  ["jquery", "lodash", "hbs", "bootstrap", "q", "firebase"],
+  function($, _, Handlebars, bootstrap, q, Firebase, dataControl,) {
+    
+  };
