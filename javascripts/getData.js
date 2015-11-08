ec3f9d3a778741855057644f32ea23c0e2af24d4
walkerdorris <walker.dorris@gmail.com>
2015-11-08 12:46:44 -0600

    getting zipcode data

--- javascripts/getData.js
+++ javascripts/getData.js
@@ -13,22 +13,54 @@ define(["jquery", "lodash", "hbs", "bootstrap", "q", "firebase"], function($, _,
 
 
 	// var getWeather = {};
-	var zipcode = 37207;
+	
+
+	// $("#loginButton").on("click", function() {
+	// 	$.ajax({
+ //   		url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us" + "&APPID=d918f1afe69b26f495f2dc4f6e949d73",
+ //  		data: {
+ //      		format: 'json'
+ //   			  },
+ //   		done: function(data2){
+ //   			console.log(data2);
+ //   		},
+ //   		error: function() {
+ //      		console.log("Error");
+ //   			  },
+ //   		type: 'GET'
+	//   });
+ //    });
+
+	
+
+	$("#loginButton").on("click", function(callback) {
+	var zipcode = {};
+	zipcode = $("#zipCode").val();
 
 	$.ajax({
-   		url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us" + "&APPID=d918f1afe69b26f495f2dc4f6e949d73",
-  		data: {
-      		format: 'json'
-   			  },
-   		done: function(data2){
-   			console.log(data2);
-   		},
-   		error: function() {
-      		console.log("Error");
-   			  },
-   		type: 'GET'
+		type: "GET",
+		url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",us" + "&APPID=d918f1afe69b26f495f2dc4f6e949d73",
+		complete: function(data) {
+			console.log("data", data);
+		},
+		success: function (data) {
+			console.log("SUCCESS");
+		},
+		fail: function() {
+			console.log("ERROR");
+		}
 	});
+});
+
+
+
+
 
 });
+
+
+
+
+
 	
 	
